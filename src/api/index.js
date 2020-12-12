50d7d7634f3a7f9d776571bde560b964636d63c2
Gabe Yuan <yugang2002@gmail.com>
2020-12-12 20:25:46 +0800

    bing

--- src/api/index.js
+++ src/api/index.js
@@ -158,8 +158,12 @@ const bingTranslate = (q, tl = "zh-Hans") => {
   return rq("https://www.bing.com/ttranslatev3", {
     method: "POST",
     headers: {
+      authority: "cn.bing.com",
       "user-agent":
         "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
+      accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+      "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
     },
     body: params,
   });
